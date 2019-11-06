449de66ed9c1114e4dbc0faf2b9b44f740f467f1
senlin.yang <senlin.yang@hand-china.com>
2019-11-06 20:23:14 +0800

    初始化项目

---
+++ src/main/java/com/theboyaply/orangeJuice/OrangeJuiceApplication.java
@@ -0,0 +1,13 @@
+package com.theboyaply.orangeJuice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrangeJuiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrangeJuiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/theboyaply/orangeJuice/OrangeJuiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theboyaply.orangeJuice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrangeJuiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

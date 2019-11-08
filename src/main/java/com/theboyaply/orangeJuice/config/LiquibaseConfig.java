3465d76ebaf4c9d9fed66ce24b818b1d1bff547e
senlin.yang <senlin.yang@hand-china.com>
2019-11-08 18:46:11 +0800

    2019-11-08->添加liquibase；MapperFacade工具类

---
+++ src/main/java/com/theboyaply/orangeJuice/config/LiquibaseConfig.java
@@ -0,0 +1,29 @@
+package com.theboyaply.orangeJuice.config;
+
+import liquibase.integration.spring.SpringLiquibase;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+/**
+ * @author theboyaply
+ * @version V1.0
+ * @Date 2019-11-8
+ * @description
+ */
+
+@Configuration
+public class LiquibaseConfig {
+    
+    @Bean
+    public SpringLiquibase liquibase(DataSource dataSource) {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setDataSource(dataSource);
+        liquibase.setChangeLog("classpath:db.liquibase/master.xml");
+        liquibase.setContexts("development,test,production");
+        liquibase.setShouldRun(true);
+        return liquibase;
+    }
+
+}

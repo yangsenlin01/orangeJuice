768811edfcd58fcafc72a7b5230b75c7f293d4ce
senlin.yang <senlin.yang@hand-china.com>
2019-11-12 12:17:58 +0800

    2019-11-12->使用数据库存储token与client

--- src/main/java/com/theboyaply/orangeJuice/config/AuthorizationServerConfig.java
+++ src/main/java/com/theboyaply/orangeJuice/config/AuthorizationServerConfig.java
@@ -1,11 +1,19 @@
 package com.theboyaply.orangeJuice.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.ClientDetailsService;
+import org.springframework.security.oauth2.provider.client.JdbcClientDetailsService;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+
+import javax.sql.DataSource;
 
 /**
  * @author theboyaply
@@ -26,15 +34,39 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
 
     @Autowired
+    private DataSource dataSource;
+
+    @Autowired
     private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Bean
+    public TokenStore tokenStore() {
+        // token交给jdbc管理
+        return new JdbcTokenStore(dataSource);
+    }
+
+    @Bean
+    public ClientDetailsService jdbcClientDetails() {
+        // client交给jdbc管理->oauth_client_details表
+        return new JdbcClientDetailsService(dataSource);
+    }
 
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-        clients.inMemory()
+        // 在内存中管理client
+        /* clients.inMemory()
                 .withClient("client")
                 .secret(bCryptPasswordEncoder.encode("secret"))
                 .authorizedGrantTypes("authorization_code")
                 .scopes("app")
-                .redirectUris("http://www.theboyaply.cn");
+                .redirectUris("http://www.theboyaply.cn");*/
+        // 交给jdbc管理client
+        clients.withClientDetails(jdbcClientDetails());
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        // token交给jdbc管理
+        endpoints.tokenStore(tokenStore());
     }
 }
--- src/test/java/com/theboyaply/orangeJuice/OrangeJuiceApplicationTests.java
+++ src/test/java/com/theboyaply/orangeJuice/OrangeJuiceApplicationTests.java
@@ -2,12 +2,17 @@ package com.theboyaply.orangeJuice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootTest
 class OrangeJuiceApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	public static void main(String[] args) {
+		System.out.println(new BCryptPasswordEncoder().encode("secret"));
 	}
 
 }

179eb20c11c8550a9192fe3acc201ffa0c5a21c4
senlin.yang <senlin.yang@hand-china.com>
2019-11-12 11:43:08 +0800

    2019-11-12->重新配置数据源

--- src/main/java/com/theboyaply/orangeJuice/OrangeJuiceApplication.java
+++ src/main/java/com/theboyaply/orangeJuice/OrangeJuiceApplication.java
@@ -3,7 +3,13 @@ package com.theboyaply.orangeJuice;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
 
 /**
  * @author theboyaply
@@ -20,4 +26,12 @@ public class OrangeJuiceApplication {
         SpringApplication.run(OrangeJuiceApplication.class, args);
     }
 
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+        // 更改spring boot默认的数据源，使用指定的配置(spring.datasource)生成一个新的数据源
+        // @Primary：当spring中存在多个相同的bean时，优先使用被@Primary注解的bean
+        return DataSourceBuilder.create().build();
+    }
 }

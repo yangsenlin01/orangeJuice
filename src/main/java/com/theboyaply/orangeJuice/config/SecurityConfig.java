8db5fbaafa1c69cbdcb7d321e1a511a3030f3d06
theBoyAply <1050696985@qq.com>
2019-11-21 01:37:11 +0800

    2019-11-21->关闭跨域拦截

--- src/main/java/com/theboyaply/orangeJuice/config/SecurityConfig.java
+++ src/main/java/com/theboyaply/orangeJuice/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.theboyaply.orangeJuice.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -17,6 +18,11 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+    }
 
     @Override
     public void configure(WebSecurity web) {

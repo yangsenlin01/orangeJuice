ed0986dfdcc817dbebf2bd82ee67deddd505cb2c
senlin.yang <senlin.yang@hand-china.com>
2019-11-21 21:22:34 +0800

    2019-11-21->/oauth/token请求跨域处理

--- src/main/java/com/theboyaply/orangeJuice/config/SecurityConfig.java
+++ src/main/java/com/theboyaply/orangeJuice/config/SecurityConfig.java
@@ -1,11 +1,14 @@
 package com.theboyaply.orangeJuice.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
+import org.springframework.web.cors.CorsUtils;
 
 /**
  * @author theboyaply
@@ -19,15 +22,29 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf();
+        /*
+         * 1-->>
+         * http.csrf();
+         * ExpressionUrlAuthorizationConfigurer<HttpSecurity>.ExpressionInterceptUrlRegistry registry = http.authorizeRequests();
+         * registry.requestMatchers(CorsUtils::isPreFlightRequest).permitAll();
+         *
+         * 2--->>
+         * http.requestMatchers().antMatchers(HttpMethod.OPTIONS, "/oauth/token", "/rest/**", "/api/**", "/**")
+         * .and()
+         * .csrf().disable();
+         */
+        http.cors().and().csrf().disable();
     }
 
     @Override
     public void configure(WebSecurity web) {
         // 放开不需要校验的路径
         web.ignoring()
+                // /oauth/token予请求
+                .antMatchers(HttpMethod.OPTIONS, "/**")
                 // 放开错误提示
                 .antMatchers("/error/**")
                 // 放开swagger2
